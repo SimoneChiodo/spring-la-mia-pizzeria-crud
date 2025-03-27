49637507ebf97388f8aac80618819447ae096e7e
SimoneChiodo <simochi.005@gmail.com>
2025-03-27 15:50:10 +0100

    Add show.html, show route

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import java.util.List;
@@ -16,11 +17,13 @@ public class PizzaController {
   @Autowired
   private PizzaRepository repository;
 
+  // HOME
   @GetMapping()
   public String index() {
       return "index";
   }
   
+  // GET
   @GetMapping("/pizza")
   public String pizza(Model model) {
     List<Pizza> pizze = repository.findAll(); // SELECT * FROM 'pizze' => lista di oggetti di tipo Pizza
@@ -28,6 +31,16 @@ public class PizzaController {
     model.addAttribute("pizze", pizze);
 
     return "pizza/pizza";
+  }
+
+  // SHOW
+  @GetMapping("/pizza/{id}")
+  public String show(@PathVariable("id") Integer id, Model model) {
+    Pizza pizza = repository.findById(id).get(); // SELECT * FROM 'pizze' WHERE id = ?
+    
+    model.addAttribute("pizza", pizza);
+
+    return "pizza/show";
   }
   
 
