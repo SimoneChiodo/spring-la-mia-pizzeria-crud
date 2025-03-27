13f51827f88a1da751fc452c66d2a8a31762132e
SimoneChiodo <simochi.005@gmail.com>
2025-03-27 16:11:49 +0100

    Add filter in pizza.html

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
@@ -43,5 +44,15 @@ public class PizzaController {
     return "pizza/show";
   }
   
+  // SEARCH
+  @GetMapping("/pizza/search")
+  public String search(@RequestParam(name = "title") String title, Model model) {
+    List<Pizza> pizze = repository.findByNomeContainingIgnoreCase(title); // SELECT * FROM 'pizze' WHERE nome LIKE '%title%'
+    
+    model.addAttribute("pizze", pizze);
+
+    return "pizza/pizza";
+  }
+
 
 }
